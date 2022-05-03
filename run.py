c1433aa59ffb110fea8e004880fde308dd3365e0
MACANNAdev <trevmc77@gmail.com>
2022-05-03 12:32:35 +0000

    Add functions to baord class

--- run.py
+++ run.py
@@ -11,5 +11,30 @@ class Board:
         self.user = user
         self.size = size
         self.board = [[ "." for x in range(size)] for y in range(size)]
-        self.guesses= []
+        self.guesses = []
         self.ships = []
+
+
+    
+
+    def print(self):
+        for row in self.board:
+            print("".join(row))
+        
+        def guess(self, x, y):
+            self.guesses.append((x, y))
+            self.board[x][y]= "X"
+
+            if (x, y) in self.ships:
+                self.board[x][y] = "*"
+                return "Hit"
+            else:
+                return "Miss"
+        
+        def add_ship(self, x, y, user= "computer"):
+            if len(self.ships) >= self.num_ships:
+                print("Error: you cannot add more ships!")
+            else:
+                self.ships.append((x, y))
+                if self.user =="player":
+                    self.board[x] [y]= "@"
